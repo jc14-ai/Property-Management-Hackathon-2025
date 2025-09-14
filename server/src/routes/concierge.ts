ad4592ca25f3a3ffd7cb932a70b8f0711e432669
PLPASIG <PLPASIG@LAPTOP-EJ76F79R>
2025-09-15 00:40:40 +0800

    added server/src folder and file structure

---
+++ server/src/routes/concierge.ts
@@ -0,0 +1,18 @@
+
+
+/*
+routes:
+    view tenant list
+    assign tenant
+    remove tenant
+    view tenant information
+    approve maintenance
+    assign staff
+    approve visitor access
+    view / track visitor
+    view maintenance process
+    view bulletin board
+    add events on the bulletin board
+    view tenant utility/billing/consumption board
+
+*/
